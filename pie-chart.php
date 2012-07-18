b078fd83d9b5d93fa17ea66497041bd1721e1740
Brend Wanders <b.wanders@xs4all.nl>
2012-07-18 12:34:01 +0000

    Replaces tabs with spaces.

--- pie-chart.php
+++ pie-chart.php
@@ -9,12 +9,12 @@
 $conf = array(
     // an array of hexadecimal color codes
     'colors' => array(
-	    'a4a4ff',
-    	'a4ffa4',
-    	'ffa4a4',
-    	'a4ffff',
-    	'ffa4ff',
-    	'ffffa4',
+        'a4a4ff',
+        'a4ffa4',
+        'ffa4a4',
+        'a4ffff',
+        'ffa4ff',
+        'ffffa4',
     )
 );
 
@@ -26,8 +26,8 @@ $conf = array(
 
 // color allocation function
 function hexcolor($image, $hex) {
-	$i = hexdec($hex);
-	return imagecolorallocate($image, 0xFF & ($i >> 0x10), 0xFF & ($i >> 0x8), 0xFF & ($i));
+    $i = hexdec($hex);
+    return imagecolorallocate($image, 0xFF & ($i >> 0x10), 0xFF & ($i >> 0x8), 0xFF & ($i));
 }
 
 /*
@@ -42,43 +42,43 @@ function hexcolor($image, $hex) {
  * being 4 spaces.
  */
 function error($message) {
-	// Determine text sizes
-	$lines = explode("\n",str_replace("\t",'   ',$message));
-	$maxline = 0;
-	$lineCount = 0;
-	foreach($lines as $line) {
-		$lineCount++;
-		$maxline = max($maxline,strlen($line));
-	}
+    // Determine text sizes
+    $lines = explode("\n",str_replace("\t",'   ',$message));
+    $maxline = 0;
+    $lineCount = 0;
+    foreach($lines as $line) {
+        $lineCount++;
+        $maxline = max($maxline,strlen($line));
+    }
 
-	// calculate image box
-	$h = (imagefontheight(2)+1) * $lineCount;
-	$w = imagefontwidth(2) * $maxline;
+    // calculate image box
+    $h = (imagefontheight(2)+1) * $lineCount;
+    $w = imagefontwidth(2) * $maxline;
 
-	// create image and prep border
-	$im = imagecreatetruecolor($w + 6, $h + 4);
-	$red = imagecolorallocate($im,255,0,0);
-	$white = imagecolorallocate($im,255,255,255);
-	$black = imagecolorallocate($im,0,0,0);
+    // create image and prep border
+    $im = imagecreatetruecolor($w + 6, $h + 4);
+    $red = imagecolorallocate($im,255,0,0);
+    $white = imagecolorallocate($im,255,255,255);
+    $black = imagecolorallocate($im,0,0,0);
 
-	// draw border and text
-	imagefilledrectangle($im,0,0,$w+6,$h+4,$red);
-	imagefilledrectangle($im,2,2,$w+3,$h+1,$white);
-	$curline = 2;
-	foreach($lines as $line) {
-		imagestring($im, 2, 4, $curline, $line,$black);
-		$curline += imagefontheight(2)+1;
-	}
+    // draw border and text
+    imagefilledrectangle($im,0,0,$w+6,$h+4,$red);
+    imagefilledrectangle($im,2,2,$w+3,$h+1,$white);
+    $curline = 2;
+    foreach($lines as $line) {
+        imagestring($im, 2, 4, $curline, $line,$black);
+        $curline += imagefontheight(2)+1;
+    }
 
-	// output image
-	header('content-type: image/png');
-	imagepng($im);
-	imagedestroy($im);
-	die();
+    // output image
+    header('content-type: image/png');
+    imagepng($im);
+    imagedestroy($im);
+    die();
 }
 
 function onError($errno , $errstr, $errfile, $errline) {
-	error("$errfile:$errline\nError No: $errno\nMessage: ".$errstr);
+    error("$errfile:$errline\nError No: $errno\nMessage: ".$errstr);
 }
 
 set_error_handler('onError');
@@ -90,9 +90,9 @@ set_error_handler('onError');
 */
 
 if(isset($_SERVER['PATH_INFO'])) {
-	$pathparts = explode('/',trim($_SERVER['PATH_INFO'],'/.png'));
-	$res = array_shift($pathparts);
-	list($_REQUEST['w'],$_REQUEST['h']) = explode('x',$res);
+    $pathparts = explode('/',trim($_SERVER['PATH_INFO'],'/.png'));
+    $res = array_shift($pathparts);
+    list($_REQUEST['w'],$_REQUEST['h']) = explode('x',$res);
 }
 
 /** Get necessary input data  **/
@@ -127,10 +127,10 @@ function render($width, $height, $data) {
     $longestKey = '';
 
     foreach($data as $key=>$value) {
-    	$slices[$key] = $value;
-    	$sum = $sum + $value;
+        $slices[$key] = $value;
+        $sum = $sum + $value;
 
-	    if(strlen($longestKey) < strlen($key)) $longestKey = $key;
+        if(strlen($longestKey) < strlen($key)) $longestKey = $key;
     }
 
     //sort slices from largest to smallest
@@ -139,7 +139,7 @@ function render($width, $height, $data) {
     $numSlices = 0;
     foreach($slices as $key=>$value) {
         $sliceColors[$key] = hexcolor($image,$conf['colors'][$numSlices % count($conf['colors'])]);
-    	$numSlices++;
+        $numSlices++;
     }
 
     /** measures **/
@@ -152,38 +152,38 @@ function render($width, $height, $data) {
     $legendColorboxSpacing = 2; //spacing between colored box and key
 
     $legendTextWidth =
-    	imagefontwidth($fontsize) * strlen($longestKey); // width of longest key in legend
+        imagefontwidth($fontsize) * strlen($longestKey); // width of longest key in legend
 
     $legendTextHeight = 
-    	imagefontheight($fontsize); //height of text in legend
+        imagefontheight($fontsize); //height of text in legend
 
     $legendColorboxSize = $legendTextHeight-1; //size of colored box in legend
 
     $legendWidth = 
-    	2*$legendPadding 
-    	+ $legendColorboxSize 
-    	+ $legendColorboxSpacing 
-    	+ $legendTextWidth; //needed width for legend
+        2*$legendPadding 
+        + $legendColorboxSize 
+        + $legendColorboxSpacing 
+        + $legendTextWidth; //needed width for legend
 
     $legendHeight = 
-    	2*$legendPadding 
-    	+ $numSlices*($legendVerticalSpacing + $legendTextHeight); //needed height for legend
+        2*$legendPadding 
+        + $numSlices*($legendVerticalSpacing + $legendTextHeight); //needed height for legend
 
     $horzPieSize = min( 
-    	$width 
-    	- 2*$graphPadding 
-    	- $graphSpacing 
-    	- $legendWidth,
-    	$height
-    	- 2*$graphPadding); //Largest pie size for horizontal layout
+        $width 
+        - 2*$graphPadding 
+        - $graphSpacing 
+        - $legendWidth,
+        $height
+        - 2*$graphPadding); //Largest pie size for horizontal layout
 
     $vertPieSize = min(
-    	$height 
-    	- 2*$graphPadding 
-    	- $graphSpacing 
-    	- $legendHeight,
-    	$width
-    	- 2*$graphPadding); //largest pie size for vertical layout
+        $height 
+        - 2*$graphPadding 
+        - $graphSpacing 
+        - $legendHeight,
+        $width
+        - 2*$graphPadding); //largest pie size for vertical layout
 
     $pieSize = max($horzPieSize, $vertPieSize); //choose best size
     $verticalLayout = $vertPieSize > $horzPieSize;
@@ -195,21 +195,21 @@ function render($width, $height, $data) {
 
     $startAngle = -90;
     foreach($slices as $key => $value) {
-    	if($value > 0) {
-    		$endAngle = $startAngle + ($value/$sum) * 360;
-    		imagefilledarc($image, $cx, $cy, $pieSize, $pieSize, $startAngle, $endAngle, $sliceColors[$key], IMG_ARC_PIE);
-    		$startAngle = $endAngle;
-    	}
+        if($value > 0) {
+            $endAngle = $startAngle + ($value/$sum) * 360;
+            imagefilledarc($image, $cx, $cy, $pieSize, $pieSize, $startAngle, $endAngle, $sliceColors[$key], IMG_ARC_PIE);
+            $startAngle = $endAngle;
+        }
     }
 
 
     /** Draw Legend **/
     if($verticalLayout) {
-    	$lx = $graphPadding;
-    	$ly = $graphPadding + $pieSize + $graphSpacing;
+        $lx = $graphPadding;
+        $ly = $graphPadding + $pieSize + $graphSpacing;
     } else {
-    	$lx = $graphPadding + $pieSize + $graphSpacing;
-    	$ly = $graphPadding;
+        $lx = $graphPadding + $pieSize + $graphSpacing;
+        $ly = $graphPadding;
     }
 
     $white = imagecolorallocate($image, 255,255,255);
@@ -223,10 +223,10 @@ function render($width, $height, $data) {
     $py = $ly + $legendPadding;
 
     foreach($slices as $key=>$value) {
-    	$color = $sliceColors[$key];
-    	imagefilledrectangle($image, $px, $py, $px+$legendColorboxSize, $py+$legendColorboxSize,$color);
-    	imagestring($image,$fontsize,$px+$legendColorboxSize+$legendColorboxSpacing, $py, $key, $black);
-    	$py += $legendVerticalSpacing + $legendTextHeight;
+        $color = $sliceColors[$key];
+        imagefilledrectangle($image, $px, $py, $px+$legendColorboxSize, $py+$legendColorboxSize,$color);
+        imagestring($image,$fontsize,$px+$legendColorboxSize+$legendColorboxSpacing, $py, $key, $black);
+        $py += $legendVerticalSpacing + $legendTextHeight;
     }
 
 
